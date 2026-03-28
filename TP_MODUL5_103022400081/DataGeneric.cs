507ec59d4a76fbe0a6926eb5fcb4fb64fb63a2bb
putriagis <putribalqisnurhayati@gmail.com>
2026-03-28 20:13:19 +0700

    menambahkan class DataGeneric

---
+++ TP_MODUL5_103022400081/DataGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL5_103022400081
+{
+    internal class DataGeneric <T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+}
--- TP_MODUL5_103022400081/Program.cs
+++ TP_MODUL5_103022400081/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("TP MODUL 5");
+﻿using System;
+using TP_MODUL5_103022400081;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("103022400081");
+        data.PrintData();
+    }
+}
